f59f56d6cd695b699c4c1172530152d965fbb5f6
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2025-05-25 14:56:00 -0300

    menssagens com hora

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,103 +1,122 @@
-const login = document.querySelector(".login")
-const loginForm = login.querySelector(".login__form")
-const loginInput = login.querySelector(".login__input")
+const login = document.querySelector(".login");
+const loginForm = login.querySelector(".login__form");
+const loginInput = login.querySelector(".login__input");
 //chat elementos
-const chat = document.querySelector(".chat")
-const chatForm = chat.querySelector(".chat__form")
-const chatInput = chat.querySelector(".chat__input")
-const chatMessages = chat.querySelector(".chat__messages")
+const chat = document.querySelector(".chat");
+const chatForm = chat.querySelector(".chat__form");
+const chatInput = chat.querySelector(".chat__input");
+const chatMessages = chat.querySelector(".chat__messages");
+//hora e minutos
+
+const getHoraMinutos = () => {
+  const dataAtual = new Date();
+  const horas = String(dataAtual.getHours()).padStart(2, "0");
+  const minutos = String(dataAtual.getMinutes()).padStart(2, "0");
+  return `${horas}:${minutos}`;
+};
 
 const colors = [
-    "cadetblue",
-    "darkgoldenrod",
-    "conrflowerblue",
-    "darkkhaki",
-    "hotpink",
-    "gold"
-]
+  "cadetblue",
+  "darkgoldenrod",
+  "cornflowerblue",
+  "darkkhaki",
+  "hotpink",
+  "gold",
+];
 
+const user = { id: "", nome: "", color: "" };
 
-const user = {id: "", nome: "", color: ""}
-
-let websocket
+let websocket;
 
 const createMessageSelfElement = (content) => {
-    const div = document.createElement("div")
-    div.classList.add("message--self")
-    div.innerHTML = content
-    return div
-}
+  const div = document.createElement("div");
+  const span = document.createElement("span");
+
+  div.classList.add("message--self");
+  span.classList.add("hora");
+  span.innerHTML = getHoraMinutos();
+  div.textContent = content;
+  div.appendChild(span);
+
+  return div;
+};
 
 const createMessageOtherElement = (content, sender, senderColor) => {
-    const div = document.createElement("div")
-    const span = document.createElement("span")
+  const div = document.createElement("div");
+  const span = document.createElement("span");
+  const p = document.createElement("p");
+  const message = document.createElement("p");
 
+  div.classList.add("message--other");
+  p.classList.add("message--sender");
+  span.classList.add("hora");
 
-    div.classList.add("message--other")
-    span.classList.add("message--sender")
-    span.style.color = senderColor
+  p.style.color = senderColor;
+  p.textContent = sender;
 
-    div.appendChild(span)
+  message.textContent = content;
 
-    span.innerHTML = sender
-    div.innerHTML += content
+  span.innerHTML = getHoraMinutos();
 
-    return div
-}
+  div.appendChild(p);
+  div.appendChild(message);
+  div.appendChild(span);
+
+  return div;
+};
 
 const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length)
-    return colors[randomIndex]
-}
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+};
 
-const processMessage = ({data}) => {
-    const {userID, userName, userColor, content} = JSON.parse(data)
-    
-    const message = userID == user.id 
-    ? createMessageSelfElement(content)
-    :
-    createMessageOtherElement(content, userName, userColor)
+const processMessage = ({ data }) => {
+  const { userID, userName, userColor, content } = JSON.parse(data);
 
-    chatMessages.appendChild(message)
+  const message =
+    userID == user.id
+      ? createMessageSelfElement(content)
+      : createMessageOtherElement(content, userName, userColor);
 
-    scrollScreen()
-}
+  chatMessages.appendChild(message);
+
+  scrollScreen();
+};
 
 const handleLogin = (event) => {
-    event.preventDefault()
-    user.id = crypto.randomUUID()
-    user.nome = loginInput.value
-    user.color = getRandomColor()
+  event.preventDefault();
+  user.id = crypto.randomUUID();
+  user.nome = loginInput.value;
+  user.color = getRandomColor();
 
-    login.style.display = "none"
-    chat.style.display = "flex"
+  login.style.display = "none";
+  chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://192.168.0.100:8080")
+  websocket = new WebSocket("ws://192.168.0.100:8080");
 
-    websocket.onopen = () => websocket.send(`Usuário: ${user.nome} entrou no chat`)
-    websocket.onmessage = processMessage
-    console.log(user)
-}
+  websocket.onopen = () =>
+    websocket.send(`Usuário: ${user.nome} entrou no chat`);
+  websocket.onmessage = processMessage;
+  console.log(user);
+};
 
 const sendMessage = (event) => {
-    event.preventDefault()
-    const message = {
-        userID: user.id,
-        userName: user.nome,
-        userColor: user.color,
-        content: chatInput.value
-    }
-    websocket.send(JSON.stringify(message))
+  event.preventDefault();
+  const message = {
+    userID: user.id,
+    userName: user.nome,
+    userColor: user.color,
+    content: chatInput.value,
+  };
+  websocket.send(JSON.stringify(message));
 
-    chatInput.value = ""
-}
+  chatInput.value = "";
+};
 
-const scrollScreen = () =>  {
-    window.screenTop({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
-    })
-}
+window.scrollTo({
+  top: document.body.scrollHeight,
+  behavior: "smooth",
+});
 
-loginForm.addEventListener("submit", handleLogin)
-chatForm.addEventListener("submit", sendMessage)
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
